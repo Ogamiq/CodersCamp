c8cf5a888dde283dba1608b1f7f91877ad5293ba
Ogamiq <miloszogaza@op.pl>
2018-08-18 13:52:28 +0200

    added initial files for basic info section

--- project4/scripts/BasicInfo.js
+++
@@ -1,6 +0,0 @@
-class BasicInfo {
-    constructor() {
-        this.descriptionL = document.querySelectorAll('basic-info-boxL basic-info-description');
-        this.descriptionR = document.querySelectorAll('basic-info-boxR basic-info-description');
-    }
-}
